92bb843342ad50fe666d5271ed5b35c1fed8ddb1
TolaV <144333100+TolaV@users.noreply.github.com>
2024-11-10 22:46:45 +0100

    Add files via upload

---
+++ task_2.3/Classes/ClassLessonItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal abstract class LessonItem
+    {
+        public string Description { get; set; }
+
+        protected LessonItem(string description)
+        {
+            Description = description ?? string.Empty;
+        }
+
+        public abstract LessonItem Clone();
+    }
+}
---
+++ task_2.3/Classes/ClassTraining.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Training
+    {
+        private LessonItem[] items;
+        private int count;
+
+        public Training()
+        {
+            items = new LessonItem[4];
+            count = 0;
+        }
+
+        public void Add(LessonItem item)
+        {
+            if (count >= items.Length)
+            {
+                Resize();
+            }
+            items[count] = item;
+            count++;
+        }
+
+        private void Resize()
+        {
+            int newSize = items.Length * 2;
+            LessonItem[] newArray = new LessonItem[newSize];
+            Array.Copy(items, newArray, items.Length);
+            items = newArray;
+        }
+
+        public bool IsPractical()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (!(items[i] is PracticalLesson))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public Training Clone()
+        {
+            var clonedTraining = new Training();
+            for (int i = 0; i < count; i++)
+            {
+                clonedTraining.Add(items[i].Clone());
+            }
+            return clonedTraining;
+        }
+
+        public void Display()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (items[i] is Lecture lecture)
+                {
+                    Console.WriteLine($"Lecture: {lecture.Description}, topic: {lecture.Topic}");
+                }
+                else if (items[i] is PracticalLesson practical)
+                {
+                    Console.WriteLine($"Practical: {practical.Description}, link to task: {practical.LinkToTheTask}, link to solution: {practical.LinkToTheSolution}");
+                }
+            }
+        }
+    }
+}
